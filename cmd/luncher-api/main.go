5bdb7b913a83ff98f02398a839c5f03ea84338b9
Roberto Sora <r.sora@arduino.cc>
2022-11-03 19:02:57 +0100

    Add bill starter kit without wiring handlers

--- cmd/luncher-api/main.go
+++ cmd/luncher-api/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"expvar"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	_ "net/http/pprof"
 
 	"github.com/ardanlabs/conf/v3"
 	"github.com/pkg/errors"
@@ -20,10 +27,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// a := App{}
-	// a.Initialize()
-	// log.Println("Starting...")
-	// a.Run(":8000")
 }
 
 func run() error {
@@ -32,7 +35,9 @@ func run() error {
 	// This is the config struct for the luncher-api app.
 	var cfg struct {
 		Web struct {
-			APIHost string `conf:default:0.0.0.0:8000`
+			APIHost         string        `conf:"default:0.0.0.0:8000"`
+			DebugHost       string        `conf:"default:0.0.0.0:8001"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
 		}
 	}
 
@@ -59,6 +64,73 @@ func run() error {
 	}
 	log.Printf("main : Config :\n%v\n", out)
 
-	return nil
+	// Start a debug server.
+	log.Println("main : debug Server : Starting")
 
+	// Automagically, both expvar and pprof packages
+	// add to the default MUX a /debug/pprof and a /debug/vars
+	// routes.
+	// Use `expvarmon` tool to inspect with pretty printing exported vars.
+	go func() {
+		log.Printf("main : Debug Listening %s", cfg.Web.DebugHost)
+		log.Printf("main : Debug Listener closed", http.ListenAndServe(cfg.Web.DebugHost, http.DefaultServeMux()))
+	}()
+
+	// Start the API service
+	log.Println("main : API Server : Starting")
+
+	// Make a channel to listen	for interrupt or Terminate signal from OS.
+	// Use a buffered channel because signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// a := App{}
+	// a.Initialize()
+	// a.Run(cfg.Web.APIHost)
+
+	api := http.Server{
+		Addr:    cfg.Web.APIHost,
+		Handler: handlers.API(shutdown, log),
+	}
+
+	// Make a channel to listen to server errors.
+	// Use a buffered channel so that the go routine can exit if
+	// we don't fetch the error.
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("main : API Listening %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+
+	}()
+
+	// Blocking main and wait for shutdown.
+	select {
+
+	case err := <-serverErrors:
+		return errors.Wrap(err, "server error")
+
+	case sig := <-shutdown:
+		log.Printf("main : %v : Shutdown started", sig)
+
+		// Give outstanding requests a deadline for completion.
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		// Asking listener to shutdown and load shed.
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", cfg.Web.ShutdownTimeout, err)
+			err = api.Close()
+		}
+
+		// Log the status of this shutdown.
+		switch {
+		case sig == syscall.SIGSTOP:
+			return errors.New("integrity issue caused shutdown")
+		case err != nil:
+			return errors.Wrap(err, "could not stop gracefully")
+		}
+	}
+	return nil
 }
