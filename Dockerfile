62ead1f610f210171b04a7e97c2f78fd6898d34f
Roberto Sora <r.sora@arduino.cc>
2022-11-03 17:50:15 +0100

    Align build files to include passing build version

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM balenalib/raspberry-pi-debian-golang:latest
 WORKDIR /go/src/app
 COPY . .
 
-RUN go build ./...
+RUN go build -ldflags "-X main.build=docker" -v ./...
 RUN go install -v ./...
 
 EXPOSE 8000
 
-CMD ["luncher"]
+CMD ["luncher-api"]
--- cmd/luncher-api/main.go
+++ cmd/luncher-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"fmt"
 	"log"
 	"os"
@@ -8,6 +9,10 @@ import (
 	"github.com/ardanlabs/conf/v3"
 	"github.com/pkg/errors"
 )
+
+// build is the git version of this program. It is set
+// using build flags during the build process.
+var build = "develop"
 
 func main() {
 	if err := run(); err != nil {
@@ -23,13 +28,16 @@ func main() {
 
 func run() error {
 	log := log.New(os.Stdout, "LUNCHER : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-	log.Println("init")
+
+	// This is the config struct for the luncher-api app.
 	var cfg struct {
 		Web struct {
 			APIHost string `conf:default:0.0.0.0:8000`
 		}
 	}
 
+	// Using ardanlabs/conf package we get OOB also the `--help`
+	// flag that prints all the config items required by the app.
 	if usage, err := conf.Parse("LUNCHER", &cfg); err != nil {
 		if err == conf.ErrHelpWanted {
 			fmt.Println(usage)
@@ -37,5 +45,20 @@ func run() error {
 		}
 		return errors.Wrap(err, "parsing config")
 	}
+
+	// Print the build version for our logs, and
+	// expose it under /debug/vars
+	expvar.NewString("build").Set(build)
+	log.Printf("main : Started : Application initializing : version %q", build)
+	defer log.Println("main : Completed")
+
+	// Print the config we are going to use.
+	out, err := conf.String((&cfg))
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main : Config :\n%v\n", out)
+
 	return nil
+
 }
