9f44304d6a372504c9e80f6154f89ef1bf7b4c31
rsora <sora.roberto@gmail.com>
2024-03-11 18:55:17 +0100

    recipes endpoint

--- app.go
+++ app.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const htmlTemplate = `<html>
+const suggestionTemplate = `<html>
 <head>
 	<style>
 	
@@ -32,7 +32,47 @@ const htmlTemplate = `<html>
 	</head>
 	
 <body>
-	<div class=rotated><h1><a href="status">{{ .Recipe }}</a></h1></div>
+	<div class=rotated><h1>{{ .Recipe }}</h1></div>
+</body>
+</html>`
+
+const recipeTemplate = `<html>
+<head>
+<style>
+body {
+  font-family: 'Courier New', monospace;
+}
+</style>
+</head>
+<body>
+	<div>
+	<h1><a href="status">Ciambellone per tutte le ore</a></h1>
+	<h2>Ingredienti</h2>
+	<ul>
+		<li>300 gr. farina 00</li>
+		<li>250 gr. zucchero semolato</li>
+		<li>200 gr. burro</li>
+		<li>4 uova  </li>
+		<li>13 gr. cremor tartaro </li>
+		<li>mandorle qb</li>
+		<li>gocce di cioccolato qb</li>
+	</ul>
+	<h2>Preparazione</h2>
+	<ol>
+		<li>Accendere il forno e tostare mandorle nel forno mentre si scalda</li>
+		<li>Scaldare burro nel microonde 10 secondi per ammorbidirlo</li>
+		<li>Mescolare burro e zucchero nella planetaria</li>
+		<li>Aggiungere le uova ad una ad una per farle incoporare bene</li>
+		<li>mescolare in una ciotola le farine, il cremor tartaro</li>
+		<li>Aggiungere le farine a mano a mano, appena scompaiono, spegnere la planetaria</li>
+	</ol>
+	<h2>Cottura</h2>
+	Forno sopra e sotto 170 C per 40 minuti
+	<h2>Note</h2>
+	<ul>
+		<li>imburrare la teglia quadrata piccola di acciaio</li>
+	</ul>
+	</div>
 </body>
 </html>`
 
@@ -53,6 +93,8 @@ func (a *App) Initialize() {
 func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/status", a.getStatus).Methods("GET")
 	a.Router.HandleFunc("/daily", a.getSuggestion).Methods("GET")
+	a.Router.HandleFunc("/recipes", a.getRecipes).Methods("GET")
+
 }
 
 func (a *App) Run(addr string) {
@@ -74,7 +116,13 @@ func (a *App) getSuggestion(w http.ResponseWriter, r *http.Request) {
 	}
 	s := time.Now().Unix() % int64(len(SuggestionList))
 	log.Println(s)
-	sp := AddSimpleTemplate(htmlTemplate, SuggestionList[s])
+	sp := AddSimpleTemplate(suggestionTemplate, SuggestionList[s])
+	respondWithHTML(w, http.StatusOK, sp)
+}
+
+func (a *App) getRecipes(w http.ResponseWriter, r *http.Request) {
+
+	sp := AddSimpleTemplate(recipeTemplate, Suggestion{})
 	respondWithHTML(w, http.StatusOK, sp)
 }
 
